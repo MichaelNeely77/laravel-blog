46944867b4df6ff408d5fea0ae36c5ce0d83c68f
Michael Neely <michael@michaelneely.net>
2020-01-05 22:35:47 -0500

    Added Post and Category models

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                <h4>Login to our Blog Admin area</h4>
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
