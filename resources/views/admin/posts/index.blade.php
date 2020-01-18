b0fd72e0695b6514a704ede5140573c40e51d12b
Michael Neely <michael@michaelneely.net>
2020-01-17 23:50:21 -0500

    Added saved posets index.blade file.

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -20,7 +20,15 @@
                 </th>
             </thead>
             <tbody>
-                
+                @foreach ($posts as $post)
+                    <tr>
+                        <td>Image</td>
+                        <td>{{ $post->title }}</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                    </tr>
+                    
+                @endforeach
             </tbody>
         </table>
     </div>
