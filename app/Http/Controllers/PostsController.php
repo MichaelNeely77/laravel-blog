3939a7a01035819d682687aea5b88f3321b44b16
Michael Neely <michael@michaelneely.net>
2020-01-22 23:08:40 -0500

    Added more functionality to the edit.blade.php for tags and acategories and exploring relationships

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -106,7 +106,10 @@ class PostsController extends Controller
     {
         $post = Post::find($id);
 
-        return view('admin.posts.edit')->with('post', $post)->with('categories', Category::all());
+        return view('admin.posts.edit')
+            ->with('post', $post)
+            ->with('categories', Category::all())
+            ->with('tags', Tag::all());
     }
 
     /**
@@ -146,6 +149,8 @@ class PostsController extends Controller
         $post->category_id = $request->category_id;
 
         $post->save();
+
+        $post->tags()->sync($request->tags);
 
         Session::flash('success', 'Post updated'); 
 
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -23,9 +23,30 @@
                 <label for="category">Select a Category</label>
                 <select name="category_id" id="" class="form-control">
                     @foreach ($categories as $category)
-                        <option value={{ $category->id }}>{{ $category->name }}</option>
+                        <option value="{{ $category->id }}"
+                                @if ($post->category->id == $category->id)
+                                    selected
+                                @endif
+                            
+                            >{{ $category->name }}</option>
                     @endforeach
                 </select>
+            </div>
+            <div class="form-group">
+                <label for="tags">Select a Tag</label>
+                @foreach ($tags as $tag)
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="tags[]" value="{{ $tag->id }}"
+                            @foreach($post->tags as $t)
+                                @if($tag->id == $t->id)
+                                    checked
+                                @endif
+                            @endforeach
+
+                            
+                            >{{ $tag->tag }}</label>
+                    </div>
+                @endforeach
             </div>
             <div class="form-group">
                 <label for="content">Content</label>
