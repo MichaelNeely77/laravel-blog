33e0245f70ed1b403bbcac67e0417e4677f2b217
Michael Neely <michael@michaelneely.net>
2020-02-03 23:13:06 -0500

    Finished adding site settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Setting;
 
+use Session;
+
 use Illuminate\Http\Request;
 
 class SettingsController extends Controller
@@ -21,6 +23,23 @@ class SettingsController extends Controller
 
     public function update() 
     {
+        $this->validate(request(), [
+            'site_name' => 'required',
+            'contact_number' => 'required',
+            'contact_email' => 'required',
+            'address' => 'required'
+        ]);
         $settings = Setting::first();
+
+        $settings->site_name = request()->site_name;
+        $settings->contact_number = request()->address;
+        $settings->contact_email = request()->contact_email;
+        $settings->contact_number = request()->contact_number;
+
+        $settings->save();
+
+        Session::flash('success', 'Settings updated');
+
+        return redirect()->back();
     }
 }
