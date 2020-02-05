48947fe2e87a716291fd892131d78a63aa1cf2d4
Michael Neely <michael@michaelneely.net>
2020-02-04 23:57:00 -0500

    Added more features to new front end

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Setting;
+
+use Illuminate\Http\Request;
+
+class FrontEndController extends Controller
+{
+    public function index()
+    {
+        return view('index')
+            ->with('title', Setting::first()->site_name);
+    }
+}
